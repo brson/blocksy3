3aa984d8a14dc91e42f4cfd52b6afa439d3a7220
Brian Anderson <andersrb@gmail.com>
2021-01-11 04:42:22 +0000

    .

--- src/index.rs
+++ src/index.rs
@@ -1,5 +1,6 @@
 pub use std::sync::Arc;
 pub use std::sync::{RwLock, RwLockWriteGuard};
+pub use std::sync::atomic::{AtomicUsize, Ordering};
 pub use std::collections::BTreeMap;
 
 #[derive(Eq, PartialEq, Ord, PartialOrd)]
@@ -19,6 +20,7 @@ pub struct Generation(pub usize);
 pub struct Address(pub usize);
 
 pub struct Index {
+    gen: AtomicUsize,
     keymap: Arc<RwLock<BTreeMap<Key, Arc<Node>>>>,
 }
 
@@ -33,19 +35,22 @@ pub struct Cursor {
     keymap: Arc<RwLock<BTreeMap<Key, Arc<Node>>>>,
 }
 
-pub struct CommitLock<'index> {
-    gen: Generation,
+pub struct Writer<'index> {
+    next_gen: Generation,
+    current_gen: &'index AtomicUsize,
     lock_guard: RwLockWriteGuard<'index, BTreeMap<Key, Arc<Node>>>,
 }
 
 impl Index {
     pub fn new() -> Index {
         Index {
+            gen: AtomicUsize::new(0),
             keymap: Arc::new(RwLock::new(BTreeMap::new())),
         }
     }
 
     pub fn read(&self, gen: Generation, key: &Key) -> Option<Value> {
+        assert!(gen <= Generation(self.gen.load(Ordering::SeqCst)));
         let map = self.keymap.read().expect("lock");
         if let Some(node) = map.get(key) {
             let history = node.history.read().expect("lock");
@@ -61,10 +66,20 @@ impl Index {
     }
 
     pub fn cursor(&self, gen: Generation) -> Cursor {
+        assert!(gen <= Generation(self.gen.load(Ordering::SeqCst)));
         Cursor {
             gen,
             current: None,
             keymap: self.keymap.clone(),
+        }
+    }
+
+    pub fn write(&self, gen: Generation) -> Writer {
+        assert!(gen >= Generation(self.gen.load(Ordering::SeqCst)));
+        Writer {
+            next_gen: gen,
+            current_gen: &self.gen,
+            lock_guard: self.keymap.write().expect("lock"),
         }
     }
 }
