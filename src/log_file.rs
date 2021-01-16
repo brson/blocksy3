4cc01878b2b70f60f6bad1a77b462393feb307b8
Brian Anderson <andersrb@gmail.com>
2021-01-16 05:34:10 +0000

    .

--- src/log_file.rs
+++ src/log_file.rs
@@ -6,21 +6,18 @@ use anyhow::Result;
 
 pub struct LogFile<Cmd> where Cmd: Serialize + for <'de> Deserialize<'de> {
     pub append: Arc<Box<dyn Fn(Cmd) -> Box<dyn Future<Output = Result<Address>> + Unpin + 'static> + Send + Sync>>,
-    pub read_at: Arc<Box<dyn Fn(Address) -> Box<dyn Future<Output = Result<(Cmd, Option<Address>)>>> + Send + Sync>>,
+    pub read_at: Arc<Box<dyn Fn(Address) -> Box<dyn Future<Output = Result<(Cmd, Option<Address>)>> + Unpin + 'static> + Send + Sync>>,
 }
 
 impl<Cmd> LogFile<Cmd>
 where Cmd: Serialize + for <'de> Deserialize<'de>
 {
     pub async fn append(&self, cmd: Cmd) -> Result<Address> {
-        let append_fn = self.append.clone();
-        let future = append_fn(cmd);
-        let res = future.await;
-        res
+        (self.append)(cmd).await
     }
 
     pub async fn read_at(&self, addr: Address) -> Result<(Cmd, Option<Address>)> {
-        Ok(panic!())
+        (self.read_at)(addr).await
     }
 }
 
